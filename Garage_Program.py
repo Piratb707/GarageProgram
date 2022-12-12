f486d3af405a80c7847419913d9e0cc121bf7e7f
PiraTb707 <beloshapkobm@yandex.ru>
2022-12-13 01:05:54 +0600

    Добавлена работа через БД SQLite3

--- Garage_Program.py
+++ Garage_Program.py
@@ -1,33 +1,57 @@
-cars = []
+import sqlite3
 
-print('*' * 10, 'Гараж v.0.0.1', '*' * 10)
+print('*' * 10, 'Гараж v.1.0.0', '*' * 10)
+
+db = sqlite3.connect('garage.db')
+sql = db.cursor()
+
+sql.execute("""CREATE TABLE IF NOT EXISTS cars(
+    car TEXT
+)
+""")
+db.commit()
 
 responce = 1
+
 while responce:
   print("""Выбериет действие:
         1 - Добавить авто
         2 - Удалить авто
         3 - Вывести список
         4 - Поиск
-        5 - Сортировка гаража
         0 - Выход""")
   responce = int(input("... "))
   if responce == 1:
     car = input("Новое авто: ")
-    cars.append(car)
+    sql.execute(f"SELECT car FROM cars WHERE car = '{car}'")
+    if sql.fetchone() is None:
+        sql.execute(f"INSERT INTO cars VALUES ('{car}')")
+        db.commit()
+
+        print("Добавлено!")
+    else:
+        print(f"Автомобиль {car} у вас уже есть!")
+
   elif responce == 2:
     car = input("Удалить авто: ")
-    cars.remove(car)
+    sql.execute(f"SELECT car FROM cars WHERE car = '{car}'")
+    if sql.fetchone() is None:
+        print(f"У вас нет автомобиля {car}")
+    else:
+        sql.execute(f"DELETE FROM cars WHERE car = '{car}'")
+        db.commit()
+
   elif responce == 3:
-    print(f'Всего машин в гараже {len(cars)} автомобилей \n{cars}')
+    cars = sql.execute("SELECT * FROM cars")
+    for i in cars:
+        print(i)
+
   elif responce == 4:
     car = input("Поиск: ")
-    if car in cars:
-      print("Такая машина есть в гараже!")
+    sql.execute(f"SELECT * FROM cars WHERE car = '{car}'")
+    if sql.fetchone() is None:
+        print(f"Автомобиль {car} отсутствует в вашем гараже!")
     else:
-      print("Такой машины нет в гараже!!!")
-  elif responce == 5:
-    cars.sort()
-    print('Отсортировано!')
+        print(f"Автомобиль {car} есть у вас в гараже!")
   else:
     print('Пока!')
